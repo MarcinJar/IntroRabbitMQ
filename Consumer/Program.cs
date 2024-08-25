715e63a3c27fc0d0323b53a5d6d92c9927f74503
Marcin Jarmułowski <m.jarmulowski@gmail.com>
2024-08-25 22:29:10 +0200

    Add init commit with Producer and Consumer console app connecterd with rabbitMQ

---
+++ Consumer/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "rabbitmqcontainer" };
+
+var number = 0;
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(
+        queue: "sample",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+
+        Console.WriteLine("Number of message: " + ++number);
+        Console.WriteLine($"[x] Received a message : {message}");
+    };
+
+    channel.BasicConsume(
+        queue: "sample",
+        autoAck: true,
+        consumer: consumer);
+
+
+    Console.WriteLine(" Press any key to exit.");
+    Console.ReadKey();
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "rabbitmqcontainer" };
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(
+        queue: "sample",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null
+    );
+
+    string message = "This is sample message";
+
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(
+        exchange: "",
+        routingKey: "sample",
+        basicProperties: null,
+        body: body
+    );
+
+    Console.WriteLine($"[x] Sent a message : {message}");
+}
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
